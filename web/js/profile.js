fd6ab95867d9c8ae60910cc74b1600b6598d44cb
GEKT0R9 <57034572+GEKT0R9@users.noreply.github.com>
2021-03-24 15:41:03 +0400

    Ntcn

--- web/js/profile.js
+++ web/js/profile.js
@@ -11,17 +11,20 @@ $(document).ready(function () {
     })
 
     $('.problem').click(function () {
-        $('.title_menu').html($(this).children()[1].innerText);
-        $('.description_menu').html($(this).children()[2].innerText);
-        $('.status_menu').html('Статус: ' + $(this).children()[3].innerText);
-        $('.category_menu').html('Категория: ' + $(this).children()[4].innerText);
-        $('.date_menu').html($(this).children()[5].innerText);
-        let img = $($(this).children()[0]).clone();
-        $(img).removeClass('hidden');
-        $('.info_menu > div.img').html($(img));
-        let id = $(this).attr('id').split('_')
-        $('.yes > input').val(id[id.length - 1]);
-        $('.hidden_id').val(id[id.length - 1]);
+        if($(this).find('.info_menu').length > 0){
+            $('.content > .info_menu').replaceWith($(this).find('.info_menu'));
+        }
+        // $('.title_menu').html($(this).children()[1].innerText);
+        // $('.description_menu').html($(this).children()[2].innerText);
+        // $('.status_menu').html('Статус: ' + $(this).children()[3].innerText);
+        // $('.category_menu').html('Категория: ' + $(this).children()[4].innerText);
+        // $('.date_menu').html($(this).children()[5].innerText);
+        // let img = $($(this).children()[0]).clone();
+        // $(img).removeClass('hidden');
+        // $('.info_menu > div.img').html($(img));
+        // let id = $(this).attr('id').split('_')
+        // $('.yes > input').val(id[id.length - 1]);
+        // $('.hidden_id').val(id[id.length - 1]);
     })
 
     $('#filt').change( function (){
