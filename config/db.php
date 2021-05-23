2f95d38e2ccb95f25488e012c1bb0f25c573e571
Владимир Крупкин <krupkin.vladimir@yandex.ru>
2021-05-23 18:51:55 +0400

    mysql to postgresql 2

--- config/db.php
+++ config/db.php
@@ -5,8 +5,9 @@ $DATABASE_URL = [
     'user' => 'postgres',
     'pass' => '',
 ];
-$DATABASE_URL = parse_url("postgres://fexvodefzkeggz:29e87820ce50be122e986f71de56e95f964ac0fadc94d746947cee807f65430d@ec2-52-19-170-215.eu-west-1.compute.amazonaws.com:5432/d91r9ph6tifk9e");
-//var_dump($DATABASE_URL);
+$url = "postgres://fexvodefzkeggz:29e87820ce50be122e986f71de56e95f964ac0fadc94d746947cee807f65430d@ec2-52-19-170-215.eu-west-1.compute.amazonaws.com:5432/d91r9ph6tifk9e";
+$DATABASE_URL = parse_url(getenv("DATABASE_URL"));
+
 return [
     'class' => 'yii\db\Connection',
     'dsn' => 'pgsql:host='.$DATABASE_URL["host"].';dbname='.ltrim($DATABASE_URL["path"], "/"),
