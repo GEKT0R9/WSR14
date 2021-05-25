aab171140ca8d8277f41b4b2ba0cff28e59c9879
Владимир Крупкин <krupkin.vladimir@yandex.ru>
2021-05-25 15:28:59 +0400

    heroku4

--- config/db.php
+++ config/db.php
@@ -5,18 +5,21 @@ $DATABASE_URL = [
     'user' => 'postgres',
     'pass' => '',
 ];
-$url = "postgres://fexvodefzkeggz:29e87820ce50be122e986f71de56e95f964ac0fadc94d746947cee807f65430d@ec2-52-19-170-215.eu-west-1.compute.amazonaws.com:5432/d91r9ph6tifk9e";
-$DATABASE_URL = parse_url($url);
+//$url = "postgres://fexvodefzkeggz:29e87820ce50be122e986f71de56e95f964ac0fadc94d746947cee807f65430d@ec2-52-19-170-215.eu-west-1.compute.amazonaws.com:5432/d91r9ph6tifk9e";
+//$DATABASE_URL = parse_url(getenv("DATABASE_URL"));
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'pgsql:host='.$DATABASE_URL["host"].';dbname='.ltrim($DATABASE_URL["path"], "/"),
-    'username' => $DATABASE_URL["user"],
-    'password' => $DATABASE_URL["pass"],
+    'dsn' => 'pgsql:host=ec2-52-19-170-215.eu-west-1.compute.amazonaws.com;dbname=d91r9ph6tifk9e',
+    'username' => 'fexvodefzkeggz',
+    'password' => '29e87820ce50be122e986f71de56e95f964ac0fadc94d746947cee807f65430d',
     'charset' => 'utf8',
-
-    // Schema cache options (for production environment)
-    //'enableSchemaCache' => true,
-    //'schemaCacheDuration' => 60,
-    //'schemaCache' => 'cache',
 ];
+
+//return [
+//    'class' => 'yii\db\Connection',
+//    'dsn' => 'pgsql:host='.$DATABASE_URL["host"].$DATABASE_URL["port"].';dbname='.ltrim($DATABASE_URL["path"], "/"),
+//    'username' => $DATABASE_URL["user"],
+//    'password' => $DATABASE_URL["pass"],
+//    'charset' => 'utf8',
+//];
